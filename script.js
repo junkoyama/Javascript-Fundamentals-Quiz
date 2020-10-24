163461127fec1a33d771320ea6c1b23170ebc737
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2020-10-24 09:43:28 -0400

    Tested the highscores button by adding querysSelector and event listener then console log

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var startButton = document.querySelector("#start");
-
+var viewHighscore = document.querySelector("#highscores")
 
 
 startButton.addEventListener("click", function() {
 console.log("Start Button is clicked!")
+})
 
+viewHighscore.addEventListener("click", function(){
+    console.log("Highscores btn clicked!")
 })
