9259fe6f638adfc343b2d6ed1b67fa143c73e8eb
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2020-10-24 15:27:51 -0400

    Added timer interval to the start button event listener

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 var startButton = document.querySelector("#start");
-var viewHighscore = document.querySelector("#highscores")
+var viewHighscore = document.querySelector("#highscores");
+var timerCount = document.querySelector(".timer");
 
+var secondsLeft = 10;
 
 startButton.addEventListener("click", function() {
 console.log("Start Button is clicked!")
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerCount.textContent = "Timer: " + secondsLeft + " seconds left";
+
+        if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+setTime();
 })
 
 viewHighscore.addEventListener("click", function(){
     console.log("Highscores btn clicked!")
 })
+
