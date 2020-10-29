e8cbbefdbaf5e6e01af676031fe28ebff4c1f2fc
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2020-10-29 16:58:35 -0400

    Added a questions and highscores sheet to separate from other files

---
+++ questions.js
@@ -0,0 +1,25 @@
+//List of quiz questions
+var questions = [ 
+    { q1: "What number does an array start counting from?",
+        choices: ["Zero", "One", "Two", "Negative One"],
+        correct: 0
+    },
+    { q2: "How does HTML read from your Javascript file?",
+        choices: ["Magic", "<script src = ''></script>", "addEventListener()", "function()"],
+        correct: 1
+    },
+    { q3: "What does '===' mean?",
+      choices: ["Not equal to", "Equal to", "Divided by", "NaN"],
+      correct: 1
+    },
+    { q4: "How do you add an element to the end of an array?",
+      choices: ["length()", "pop()", "push()", "!=="],
+      correct: 2
+
+    },
+    { q5: "What are the different ways to define a variable in Javascript?",
+      choices: ["v1, v2, v3", "function()", "var, const and let", "You cannot"],
+      correct: 2
+    }
+];
+//End of quiz questions list
